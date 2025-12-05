fed5e12fc18e59a0061eabec157705724434b90f
Milton Soares <eng.milton.soares@gmail.com>
2025-12-05 11:38:43 -0300

    starting login manager

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SCRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
+
 db.init_app(app)
+login_manager.init_app(app)
 
 @app.route('/', methods=['GET'])
 def hello():
