cc62b65986420a20b8d2f179f57019723713021c
Milton Soares <eng.milton.soares@gmail.com>
2025-12-05 10:12:08 -0300

    setting database configs

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
 
 app = Flask(__name__)
-app.config['SCRET_KEY'] = "key"
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SCRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 @app.route('/', methods=['GET'])
 def hello():
--- models/user.py
+++ models/user.py
@@ -1,12 +1,8 @@
-from app import db
+from database import db
+from flask_login import UserMixin
 
-
-class User(db.Model):
+class User(db.Model, UserMixin):
     # id (int), username (string), password(string)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-
-    
-    def __init__(self):
-        super().__init__()
