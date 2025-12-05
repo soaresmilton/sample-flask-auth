29f77d6466f992dbaba283759c5d5a7aa92ffdc8
soaresmilton <eng.milton.soares@gmail.com>
2025-12-05 19:06:14 -0300

    updating authentication flow

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
-app.config['SCRET_KEY'] = "your_secret_key"
+app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 login_manager = LoginManager()
@@ -12,9 +12,47 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
-@app.route('/', methods=['GET'])
-def hello():
-    return 'Hello World'
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = db.session.execute(db.select(User).filter_by(username=username)).scalar_one_or_none() # Modo legado: User.query.filter_by(username=username).first()
+        
+        if user and user.password == password:
+            login_user(user)
+            return jsonify({"message": "Autenticação realizada com sucesso."})
+            
+    return jsonify({"message": "Credenciais inválidas."}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuário cadastrado com sucesso."}) 
+
+    return jsonify({"message": "Credenciais inválidas."}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
