ed3cca351b3473f33b4f90e0186aeb3fed8e642a
soaresmilton <eng.milton.soares@gmail.com>
2025-12-06 15:16:13 -0300

    add criptography to passwords

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -27,7 +28,7 @@ def login():
     if username and password:
         user = db.session.execute(db.select(User).filter_by(username=username)).scalar_one_or_none() # Modo legado: User.query.filter_by(username=username).first()
         
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             return jsonify({"message": "Autenticação realizada com sucesso."})
             
@@ -46,7 +47,8 @@ def create_user():
     password = data.get("password")
     
     if username and password:
-        user = User(username=username, password=password, role='user')
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "Usuário cadastrado com sucesso."}) 
